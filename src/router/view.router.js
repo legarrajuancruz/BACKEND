19d20db8088d34b6383209b70dabc63f4e1f5607
juancruzlegarra <legarrajuan@gmail.com>
2023-08-01 20:31:51 -0300

    Estilado handlebars

--- src/router/view.router.js
+++ src/router/view.router.js
@@ -4,13 +4,8 @@ import ProductManager from "../controllers/ProductManager.js";
 
 const products = new ProductManager();
 
-// router.get("/", (req, res) => {
-//   res.render("index", {});
-// });
-
 router.get("/", async (req, res) => {
   let allProducts = await products.getProducts();
-  console.log(allProducts);
   res.render("home", {
     title: "Listado de productos - handlebars",
     products: allProducts,
