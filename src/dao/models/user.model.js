fa577756227ee6e0fd376142b7dbe7c90fe43f4e
juancruzlegarra <legarrajuan@gmail.com>
2023-09-05 22:56:18 -0300

    work

--- src/dao/models/user.model.js
+++ src/dao/models/user.model.js
@@ -5,7 +5,11 @@ const collection = "users";
 const userSchema = new mongoose.Schema({
   first_name: String,
   last_name: String,
-  email: String,
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
   age: Number,
   password: String,
 });
