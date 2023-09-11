285c506198b283149140bf88fd23e8cae4d056c4
juancruzlegarra <legarrajuan@gmail.com>
2023-09-10 21:58:36 -0300

    work

--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -5,34 +5,36 @@ const sessionsRouter = Router();
 
 const UM = new UserManager();
 
-SessionsRouter.post("/login", async (req, res) => {
+sessionsRouter.post("/login", async (req, res) => {
   let { user, pass } = req.body;
   console.log(user, pass);
   const userLogged = await UM.login(user, pass);
   console.log("USER LOGIN");
   console.log(userLogged);
+
   if (userLogged) {
-    res.send({ status: "OK", message: userLogged });
+    req.session.user = {
+      name: userLogged.first_name,
+      last_name: userLogged.last_name,
+      email: userLogged.email,
+      password: userLogged.password,
+      age: userLogged.age,
+      role: userLogged.role,
+    };
+    res.send({
+      status: "OK",
+      message: "Logueo Exitoso",
+      payload: req.session.user,
+    });
   } else {
     res
       .status(401)
       .send({ status: "Error", message: "No se pudo loguaer el usuario" });
   }
   //DAMOS DE ALTA LA SESION
-  req.session.user = {
-    name: `${userLogged.first_name} ${userLogged.last_name}`,
-    email: userLogged.email,
-    age: userLogged.age,
-  };
-
-  res.send({
-    status: "success",
-    payload: req.session.user,
-    message: "Primer logueo realizado",
-  });
 });
 
-SessionsRouter.post("/register", async (req, res) => {
+sessionsRouter.post("/register", async (req, res) => {
   const { first_name, last_name, email, age, password } = req.body;
 
   const exist = await UM.leerUsuarios(email);
