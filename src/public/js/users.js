16d65f0a38e073e734b846e65793e1fabe43748d
juancruzlegarra <legarrajuan@gmail.com>
2023-09-30 02:43:16 -0300

    passport-cookie-jwt

---
+++ src/public/js/users.js
@@ -0,0 +1,21 @@
+// function llamarApi() {
+//   console.log("Llamando a api users");
+
+//   fetch("/api/users/65051a60a7efd9b588fa9bf3", {
+//     method: "GET",
+//     headers: {
+//       "Content-type": "application/json; charset=utf-8",
+//       Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+//     },
+//   }).then((result) => {
+//     if (result.status === 200) {
+//       result.json().then((json) => {
+//         console.log(json);
+//       });
+//     } else if (result.status === 401) {
+//       console.log(result);
+//       alert("Login invalido, controla tus credenciales");
+//     }
+//   });
+// }
+// llamarApi();
