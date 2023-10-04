5b2e49ea3b5fb6ccd8e65e19bfe70a5e7048dd03
juancruzlegarra <legarrajuan@gmail.com>
2023-10-03 22:45:06 -0300

    Desafio 11

--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -1,20 +1,30 @@
 import { Router } from "express";
 import passport from "passport";
-import sessions from "../controllers/sessions.controller.js";
+import { generateJWToken } from "../utils.js";
 
 const sessionsRouter = Router();
 
 sessionsRouter.get(
   "/github",
   passport.authenticate("github", { scope: ["user:email"] }),
-  sessions.users
+  async (req, res) => {}
 );
 
 //Githubcallback
 sessionsRouter.get(
   "/githubcallback",
   passport.authenticate("github", { failureRedirect: "/github/error" }),
-  sessions.github
+  async (req, res) => {
+    const user = req.user;
+
+    req.session.user = {
+      name: `${user.first_name} ${user.last_name}`,
+      email: user.email,
+      age: user.age,
+    };
+    req.session.admin = true;
+    res.redirect("/users/profile");
+  }
 );
 
 sessionsRouter.post(
@@ -22,7 +32,19 @@ sessionsRouter.post(
   passport.authenticate("login", {
     failureRedirect: "/api/sessions/fail-login",
   }),
-  sessions.login
+  async (req, res) => {
+    const user = req.user;
+    console.log("Usuario encontrado para login:");
+    console.log(user);
+
+    if (!user)
+      return res
+        .status(401)
+        .send({ status: "error", error: "credenciales incorrectas" });
+    const access_token = generateJWToken(user);
+    console.log(access_token);
+    res.send({ access_token: access_token });
+  }
 );
 
 sessionsRouter.post(
@@ -30,13 +52,29 @@ sessionsRouter.post(
   passport.authenticate("register", {
     failureRedirect: "/api/sessions/fail-register",
   }),
-  sessions.failRegister
+  async (req, res) => {
+    console.log("Nuevo usuario registrado");
+    res
+      .status(201)
+      .send({ status: "success", message: "Usuario creado con exito" });
+  }
 );
 
-sessionsRouter.post("/logout", sessions.logout);
+sessionsRouter.post("/logout", async (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.redirect("/");
+    }
+    response.redirect("/users/login");
+  });
+});
 
-sessionsRouter.get("/fail-register", sessions.failRegister);
+sessionsRouter.get("/fail-register", (req, res) => {
+  res.status(401).send({ error: "Error al procesar el registro" });
+});
 
-sessionsRouter.get("/fail-login", sessions.failLogin);
+sessionsRouter.get("/fail-login", (req, res) => {
+  res.status(401).send({ error: "Error al procesar el login" });
+});
 
 export default sessionsRouter;
