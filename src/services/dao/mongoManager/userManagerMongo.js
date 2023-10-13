fc4bdd3e86d480a0cd5f4de75cb5c19f68f73d88
juancruzlegarra <legarrajuan@gmail.com>
2023-10-13 16:14:00 -0300

    Validar archivos

--- src/services/dao/mongoManager/userManagerMongo.js
+++ src/services/dao/mongoManager/userManagerMongo.js
@@ -21,6 +21,19 @@ export default class UserService {
     return result;
   };
 
+  getAll = async () => {
+    try {
+      const users = await userModel.find();
+
+      if (users) {
+        return users;
+      }
+      return false;
+    } catch (error) {
+      return false;
+    }
+  };
+
   login = async (email) => {
     try {
       console.log(email);
