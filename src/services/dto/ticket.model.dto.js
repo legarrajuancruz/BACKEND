9f1ac6e67a8428cfe3d6dd9fe9a889a313e9476b
juancruzlegarra <legarrajuan@gmail.com>
2023-10-23 22:20:03 -0300

    Validar archivos

--- src/services/dto/ticket.model.dto.js
+++
@@ -1,9 +0,0 @@
-export default class ProductDTO {
-  constructor(user) {
-    (this.first_name = user.first_name),
-      (this.last_name = user.last_name),
-      (this.username = user.mail),
-      (this.age = user.age),
-      (this.password = user.password);
-  }
-}
