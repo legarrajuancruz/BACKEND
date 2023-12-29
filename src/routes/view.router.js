34a011ebed4b233ac475317af8b49b6e214caf09
juancruzlegarra <legarrajuan@gmail.com>
2023-12-29 02:56:39 -0300

    Validar Archivos

--- src/routes/view.router.js
+++ src/routes/view.router.js
@@ -42,7 +42,7 @@ router.get(
     const allProducts = await productService.getProducts(req.query);
     const user = req.user;
     if (user.role != "user") {
-      res.render("realtimeproducts", { allProducts, user });
+      res.render("realTimeProducts", { allProducts, user });
     } else {
       res.render("error");
     }
